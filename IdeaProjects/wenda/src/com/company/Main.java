e8475c3168586e2bfc6b4d1d163e6b9ab2033198
Wuqingwei <539877652@qq.com>
2017-08-02 16:11:14 +0800

    hello 3

--- IdeaProjects/wenda/src/com/company/Main.java
+++ IdeaProjects/wenda/src/com/company/Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("hello wuqingwei");
         System.out.print("hello wuqingwei2");
-        System.out.print("dada");
     }
 }
